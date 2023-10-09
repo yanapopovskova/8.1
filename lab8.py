7cfeaf41dfbd6e07fa097d0a7af07c76b948ff64
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-10-09 21:52:21 +0400

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -11,18 +11,23 @@ def calculate_minimum():
 
     min_sum = float('inf')
     min_combination = None
+    count_combinations = 0
 
     for combination in K:
-        # Проверяем ограничения на четность и нечетность элементов
         if all(x % 2 == 1 for x in combination[::2]) and all(x % 2 == 0 for x in combination[1::2]):
-            # Вычисляем значение целевой функции
             sum_mod = sum(abs(x) for x in combination) % 6
-            # Обновляем минимальное значение и комбинацию переменных
             if sum_mod < min_sum:
                 min_sum = sum_mod
                 min_combination = combination
+            count_combinations += 1
 
+    # Закрытие предыдущего окна результатов (если оно было)
+    if hasattr(calculate_minimum, 'result_root') and calculate_minimum.result_root.winfo_exists():
+        calculate_minimum.result_root.destroy()
+
+    # Создание нового окна результатов
     result_root = tk.Toplevel(root)
+    calculate_minimum.result_root = result_root # Сохраняем ссылку на окно результатов
     result_root.title("Результаты")
     result_root.geometry("400x300")
 
@@ -30,15 +35,16 @@ def calculate_minimum():
     result_text.pack()
 
     result_text.insert(tk.END, f"Минимальная сумма: {min_sum}\n"
+                               f"Количество допустимых комбинаций: {count_combinations}\n"
                                f"Комбинация переменных: {min_combination}\n")
-
 
 # Создание графического интерфейса
 root = tk.Tk()
 root.title("Вычисление минимума")
 root.geometry("400x300")
 
-entry_label = tk.Label(root, text="Введите число:")
+entry_label = tk.Label(root, text="Введите число n, "
+                                  "количество переменных:")
 entry_label.pack()
 
 entry = tk.Entry(root)
